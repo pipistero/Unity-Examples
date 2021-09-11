f020ecd2fab09891f0b62ce09dfd89c1853ce883
pipistero <laweircompany@gmail.com>
2021-09-11 17:46:26 +0300

    Modified Scriptable Objects example

--- Scriptable Objects/Bow.cs
+++ Scriptable Objects/Bow.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Bow", menuName = "Weapon/Bow")]
 public class Bow : ScriptableObject
 {
+	[Space]
+	[Header("Information")]
 	[SerializeField] private string _name;
 	[SerializeField] private string _description;
 
+	[Space]
+	[Header("Image")]
 	[SerializeField] private Sprite _sprite;
 
+	[Space]
+	[Header("Parameters")]
 	[SerializeField] private int _damage;
 	[SerializeField] private float _shotPrepareTime;
 	[SerializeField] private float _reloadTime;
--- Scriptable Objects/CrossBow.cs
+++ Scriptable Objects/CrossBow.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Crossbow", menuName = "Weapon/Crossbow")]
 public class CrossBow : ScriptableObject
 {
+	[Space]
+	[Header("Information")]
 	[SerializeField] private string _name;
 	[SerializeField] private string _description;
 
+	[Space]
+	[Header("Image")]
 	[SerializeField] private Sprite _sprite;
 
+	[Space]
+	[Header("Parameters")]
 	[SerializeField] private int _damage;
 	[SerializeField] private float _shotPrepareTime;
 	[SerializeField] private float _reloadTime;
