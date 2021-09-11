8645a8ebb4c30a141158019e8d3b56fbb7fe83cf
pipistero <laweircompany@gmail.com>
2021-09-11 16:55:13 +0300

    Added Camera Follow Target example

---
+++ Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Space]
+    [Header("Target")]
+    [SerializeField] private Transform _target;
+
+    [Space]
+    [Header("Parameters")]
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _timeToReachTarget = 2f;
+
+    private void FixedUpdate()
+    {
+        Vector3 offsetTargetPosition = _target.position + _offset;
+        Vector3 newCameraPosition = Vector3.Lerp(transform.position, offsetTargetPosition, (_timeToReachTarget * Time.fixedDeltaTime));
+
+        transform.position = newCameraPosition;
+    }
+}
